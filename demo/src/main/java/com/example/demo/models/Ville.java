f95cd0893e94ac14a408ceab42733a140a8df6b0
¨ProjectD20 <projectdev0220@gmail.com>
2020-06-19 17:51:02 +0100

    Create Ville Model

---
+++ demo/src/main/java/com/example/demo/models/Ville.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Mahmoud
+ *
+ */
+@Entity	
+public class Ville {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String nomVille;
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNomVille() {
+		return nomVille;
+	}
+
+
+	public void setNomVille(String nomVille) {
+		this.nomVille = nomVille;
+	}
+
+
+	/**
+	 * 
+	 */
+	public Ville() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
