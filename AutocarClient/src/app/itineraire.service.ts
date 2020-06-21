cb61138ee161544cce0689a88a675d4c927811a8
¨ProjectD20 <projectdev0220@gmail.com>
2020-06-21 11:42:00 +0100

    itineraire service ajouter rechercher itineraire method

--- AutocarClient/src/app/itineraire.service.ts
+++ AutocarClient/src/app/itineraire.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItineraireService {
+    private ItiniraireUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+    this.ItiniraireUrl = 'http://localhost:8080/GetItiniraires' ;
+  }
+
+  public RechercherItiniraires(): Observable<any[]> {
+    return this.http.get<any[]>(this.ItiniraireUrl
+, {
+      params: {
+Destination: 'id1234',
+Source: 'id1234',
+min: 'id1234',
+MAX: '5'
+      }
+    }
+);
+  }
 }
